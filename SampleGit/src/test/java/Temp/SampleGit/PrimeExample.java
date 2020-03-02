3c94b2bacbcf51edf4efe264f824527ae2c7178e
PC1 <PC1@DESKTOP-UG95BRG>
2020-03-02 12:29:18 +0530

    vi

--- SampleGit/src/test/java/Temp/SampleGit/PrimeExample.java
+++ SampleGit/src/test/java/Temp/SampleGit/PrimeExample.java
@@ -19,6 +19,24 @@ public class PrimeExample{
 	  }//end of else  
 	  }
 	 }
+	 
+	 int i,m=0,flag=0;      
+	  int vi=3;//it is the number to be checked    
+	  m=n/2;      
+	  if(n==0||n==1){  
+	   System.out.println(vi+" is not prime number");      
+	  }else{  
+	   for(i=2;i<=m;i++){      
+	    if(vi%i==0){      
+	     System.out.println(vi+" is not prime number");      
+	     flag=1;      
+	     break;      
+	    }      
+	   }      
+	   if(flag==0)  { System.out.println(vi+" is prime number"); 
+	  }//end of else  
+	  }
+	 }
 }
 	 
 	 
